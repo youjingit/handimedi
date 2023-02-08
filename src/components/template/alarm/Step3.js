752657d5a4d8a6c1c8f717c93e4b75c8fa0e292d
youjin <youjindev@gmail.com>
2023-02-09 01:30:23 +0900

    deploy

--- src/components/template/alarm/Step3.js
+++ src/components/template/alarm/Step3.js
@@ -3,27 +3,30 @@ import BottomNav from "../../fragments/BottomNav";
 import TopNav from "../../fragments/TopNav";
 
 function AlarmStep3() {
-  return (
-    <div>
-      <header>
-        <TopNav />
-      </header>
-      <main>
-        <div className="container">
-          <h2>복용 시간이 되면 알려드릴게요!</h2>
-          <div>
-            <img src="/static/images/alarm_step3.gif" alt="알림등록완료" />
-          </div>
-          <Link to="/alarm/list" className="list_btn">
-            알림 목록 보기
-          </Link>
-        </div>
-      </main>
-      <footer>
-        <BottomNav activeMenu="alarm" />
-      </footer>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<TopNav />
+			</header>
+			<main>
+				<div className="container">
+					<h2>복용 시간이 되면 알려드릴게요!</h2>
+					<div>
+						<img
+							src={`${process.env.PUBLIC_URL}/static/images/alarm_step3.gif`}
+							alt="알림등록완료"
+						/>
+					</div>
+					<Link to="/alarm/list" className="list_btn">
+						알림 목록 보기
+					</Link>
+				</div>
+			</main>
+			<footer>
+				<BottomNav activeMenu="alarm" />
+			</footer>
+		</div>
+	);
 }
 
 export default AlarmStep3;
--- src/components/template/photo/Step2.js
+++ src/components/template/photo/Step2.js
@@ -3,29 +3,32 @@ import { useNavigate } from "react-router-dom";
 import TemplateNavbar from "./Navbar";
 
 function PhotoStep2({ onPrevClick }) {
-  let navigate = useNavigate();
+	let navigate = useNavigate();
 
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/photo/step3");
-    }, 3400);
-  }, [navigate]);
-  return (
-    <div>
-      <header>
-        <TemplateNavbar onArrowClick={onPrevClick} />
-      </header>
-      <main>
-        <div className="container">
-          <h2>약을 찾고있어요...</h2>
-          <div className="loading_image">
-            <img src="/static/images/loading.gif" alt="사진검색중" />
-          </div>
-        </div>
-      </main>
-      <footer></footer>
-    </div>
-  );
+	useEffect(() => {
+		setTimeout(() => {
+			navigate("/photo/step3");
+		}, 3400);
+	}, [navigate]);
+	return (
+		<div>
+			<header>
+				<TemplateNavbar onArrowClick={onPrevClick} />
+			</header>
+			<main>
+				<div className="container">
+					<h2>약을 찾고있어요...</h2>
+					<div className="loading_image">
+						<img
+							src={process.env.PUBLIC_URL + "/static/images/loading.gif"}
+							alt="사진검색중"
+						/>
+					</div>
+				</div>
+			</main>
+			<footer></footer>
+		</div>
+	);
 }
 
 export default PhotoStep2;
